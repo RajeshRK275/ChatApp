c1329f2d94ea804e7f21ef5846084058891685b9
Rajeshkumar_27 <rajesh@leaguex.com>
2024-02-14 00:26:54 +0530

    Mistakenly added node modules

---
+++ Server/Controllers/userController.js
@@ -0,0 +1,6 @@
+const userModel = require("../Models/userModel");
+const registerUser = (req, res) => {
+  res.send("Register");
+};
+
+module.exports = { registerUser };
---
+++ Server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoute = require("./Routes/userRoute")
+
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/users",userRoute);
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Chat APIs..!")
+});
+
+
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+
+app.listen(port, (req, res) => {
+  console.log(`Server running on port : ${port}`);
+});
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection extablished"))
+  .catch((error) => console.log("MongoDB Connection Failed :", error.message));
