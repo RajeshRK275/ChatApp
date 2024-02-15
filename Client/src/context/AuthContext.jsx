dce394c4c2d2cf8d782817ae4583e7827c61b64a
Rajeshkumar_27 <rajesh@leaguex.com>
2024-02-15 06:12:07 +0530

    User Registration Frontend and Integration Works

---
+++ Client/src/context/AuthContext.jsx
@@ -0,0 +1,56 @@
+import { useCallback } from "react";
+import { createContext, useState } from "react";
+import { baseUrl, postRequest } from "../utils/services";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [registerError, setRegisterError] = useState(null);
+  const [isregisterLoading, setIsRegisterLoading] = useState(false);
+  const [registerInfo, setRegisterInfo] = useState({
+    name: null,
+    email: "",
+    password: "",
+  });
+
+  console.log("registerInfo", registerInfo);
+
+  const updateRegisterInfo = useCallback((info) => {
+    setRegisterInfo(info);
+  }, []);
+
+  const registerUser = useCallback(async (e) => {
+    e.preventDefault();
+    setIsRegisterLoading(true);
+    setRegisterError(null);
+
+    const response = await postRequest(
+      `${baseUrl}/users/register`,
+      JSON.stringify(registerInfo)
+    );
+
+    setIsRegisterLoading(false)
+
+    if (response.error) {
+      return setRegisterError(response);
+    }
+    localStorage.setItem("User",JSON.stringify(response))
+    setUser(response)
+  }, [registerInfo]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        registerInfo,
+        updateRegisterInfo,
+        registerUser,
+        registerError,
+        isregisterLoading,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
